0f37571c4431eca45d7956c4e11323d6858377a7
Al <albarrentine@gmail.com>
2023-07-14 03:05:21 -0400

    [fix] adding __repr__ and __str__ and fixing indentation in TokenType generated enum

--- template/{% if python_binding %}python{% endif %}/gen_token_types.sh
+++ template/{% if python_binding %}python{% endif %}/gen_token_types.sh
@@ -9,5 +9,14 @@ EOF
 
 IFS=$'\n'
 for line in $(cat $filename | grep "^#define [A-Z_]\+ [0-9]\+"); do
-    echo $line | sed -e 's/^#define *//g' -e 's/TOKEN_TYPE_//g' -e 's/ *$//' -e 's/\/[\/\*].*//' -e 's/ / = /g' -e 's/^/   /g'
+    echo $line | sed -e 's/^#define *//g' -e 's/TOKEN_TYPE_//g' -e 's/ *$//' -e 's/\/[\/\*].*//' -e 's/ / = /g' -e 's/^/    /g'
 done
+
+cat << EOF
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+EOF
